dc74efcc22b6c232409f1a71d5b1d2cb0699904c
Henrique <h.ferreira2004augusto@gmail.com>
2024-09-04 22:11:45 -0300

    Primeiro COMMIT

---
+++ cad-categorias.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <h2 class="bi bi-film"><i class="bi bi-plus"></i> Cadastro de Categoria</h2>
+
+    <form action="index.php?menu=inserir-categorias" method="post">
+        <div class="mb-3 col-6">>
+            <label class="form-label" for="nomeCategoria">Nome da Categoria</label>
+            <div class="input-group">
+                <div class="input-group-text">
+                    <i class="bi bi-film"></i>
+                </div>
+                <input class="form-control" type="text" name="nomeCategoria" id="nomeCategoria">
+            </div>
+            <div>
+                <input type="submit" value="Salvar">
+            </div>
+    </form>
---
+++ lista-clientes.php
@@ -0,0 +1,65 @@
+<div class="container">
+    <h3 class="sbi bi-film">Lista de Clientes</h3>
+    <div>
+        <div class="mb-3">
+            <a class="btn btn-secondary" href="index.php?menu=cad-clientes">Cadastrar nova Cliente</a>
+        </div>
+        <div class="mb-3">
+            <?php
+    //$txtPesquisa = (isset($_POST["txtPesquisa"]))?$_POST["txtPesquisa"]:"";
+    if (isset($_POST["txtPesquisa"])) {
+        $txtPesquisa = $_POST["txtPesquisa"];
+    } else {
+        $txtPesquisa = "";
+    }
+    ?>
+            <form action="" method="post">
+                <div class="input-group">
+                    <div class="input-group-text">Pesquisar</div>
+                    <input class="form-control" type="search" name="txtPesquisa" id="txtPesquisa"
+                        value="<?= $txtPesquisa ?>">
+                    <button class="btn btn-secondary" type="submit"><i class="bi bi-search"></i></button>
+                </div>
+            </form>
+        </div>
+        <div class="border p-1 rounded bg-dark">
+            <table class="table table-dark table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Nome da Cliente</th>
+                        <th>Telefone</th>
+                        <th>E-Mail</th>
+                        <th>Editar</th>
+                        <th>Excluir</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+        $sql = "SELECT * FROM tbclientes 
+        where nomeCliente like '%{$txtPesquisa}%'";
+        $rs = mysqli_query($conexao, $sql);
+        while ($dados = mysqli_fetch_assoc($rs)) {
+        ?>
+                    <tr>
+                        <td><?= $dados["idCliente"] ?></td>
+                        <td><?= $dados["nomeCliente"] ?></td>
+                        <td><?= $dados["telefoneCliente"] ?></td>
+                        <td><?= $dados["emailCliente"] ?></td>
+                        <td><a class="btn btn-outline-warning"
+                                href="index.php?menu=editar-clientes&idCliente=<?=$dados["idCliente"]?>"><i class="bi bi-pencil-square"></i></a>
+                        </td>
+                        <td><a class="btn btn-outline-danger"
+                                href="index.php?menu=excluir-clientes&idCliente=<?=$dados["idCliente"]?>"><i class="bi bi-trash"></i></a>
+                                </td>
+                </a>
+                </td>
+                    </a>
+                </td>
+                </tr>
+                <?php
+                }
+        ?>
+            </tbody>
+
+        </table>
